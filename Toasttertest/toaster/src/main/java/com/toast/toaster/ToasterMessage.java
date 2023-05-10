21c617998c040fefe3e169c850dec1516409fafe
imarul16 <63310645+imarul16@users.noreply.github.com>
2023-05-10 12:05:40 +0530

    no message

---
+++ Toasttertest/toaster/src/main/java/com/toast/toaster/ToasterMessage.java
@@ -0,0 +1,13 @@
+package com.toast.toaster;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void show(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
