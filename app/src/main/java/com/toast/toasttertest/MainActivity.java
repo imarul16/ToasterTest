3cf04ae7c1e2e567af907d0ce02d995e16debc07
Arul <arul.harsh@volgainfotech.com>
2023-05-11 20:10:47 +0530

    no message

--- app/src/main/java/com/toast/toasttertest/MainActivity.java
+++ app/src/main/java/com/toast/toasttertest/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.toast.toaster.ToasterMessage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToasterMessage.show(MainActivity.this,"bhaala");
     }
 }
